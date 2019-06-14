8f89af474c3e06cf16fe2bceae2769ffb3c515af
Eric Tovar <tovareric@gmail.com>
2019-06-14 10:13:49 -0500

    changes field name

--- main_app/models.py
+++ main_app/models.py
@@ -13,7 +13,7 @@ SKILL_LEVELS = (
 
 class Skill(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField(max_length=250)
+    skill = models.CharField(max_length=100)
     skill_level = models.IntegerField(
         choices=SKILL_LEVELS,
         default=1
